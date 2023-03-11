af2b7966086e7a09dd6bd8108a3ce38246c40d57
Andreas <and415@gmail.com>
2023-03-11 17:35:10 -0500

    setup folder structure

---
+++ app/routes/government/index.tsx
@@ -0,0 +1,9 @@
+
+export default () => {
+    // Get all grant-business matchings through firebase query
+    // Create a table with filters to display the data
+
+    return (<div>
+        Government grants hereç
+    </div>)
+}
