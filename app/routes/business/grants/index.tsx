cf2b60a18d3f49483442e5b7311be31554e9047d
Andreas <and415@gmail.com>
2023-03-13 19:46:22 -0400

    two views files

---
+++ app/routes/business/grants/index.tsx
@@ -0,0 +1,44 @@
+
+export default () => {
+    // Get all grant-business matchings through firebase query
+    // Create a table with filters to display the data
+
+    const mockData = [
+        {
+            grant: "Small business grant 123",
+            business: "Andy's Drugs"
+        },
+        {
+            grant: "Small business grant 234",
+            business: "Andy's Drugs"
+        },
+        {
+            grant: "Small business grant 34123",
+            business: "Andy's Drugs"
+        },
+        {
+            grant: "Small business grant sdgd",
+            business: "Andy's Drugs"
+        },
+    ]
+    return (<div>
+        <table className="table w-full hover">
+            <thead>
+                <tr>
+                    <th>Business Name</th>
+                    <th>Grant Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    mockData.map((data, idx) => {
+                        return <tr className="hover">
+                            <td key={idx}>{data.grant}</td>
+                            <td key={idx}>{data.business}</td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+    </div>)
+}
