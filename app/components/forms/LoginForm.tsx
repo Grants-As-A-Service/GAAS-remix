0daf474963aa1c90167048446aea850688d248cf
Magnus Reeves <rreeves8@uwo.ca>
2023-03-13 12:55:39 -0400

    error handling fix

--- app/components/forms/LoginForm.tsx
+++ app/components/forms/LoginForm.tsx
@@ -1,11 +1,11 @@
 import { FormButton, FormInput, Form } from "./Form";
-import { useFormValidationPost, validEmail, validPassword } from "../hooks/formValidation";
+import { useFormValidation, validEmail, validPassword } from "../hooks/formValidation";
 import { authorize, login, signup } from "~/utils/firebaseClient";
 
 type UserLogin = { email: string; password: string };
 
 export default function Login({ done }: { done: () => void }) {
-    const [update, submit, error, setError] = useFormValidationPost<UserLogin>(
+    const [update, error, setError, submit] = useFormValidation<UserLogin>(
         {
             email: "",
             password: "",
@@ -21,8 +21,13 @@ export default function Login({ done }: { done: () => void }) {
             return "";
         },
         async (state: UserLogin) => {
-            await login(state.email, state.password);
-            return authorize();
+            try {
+                await login(state.email, state.password);
+                await authorize();
+                done();
+            } catch (error) {
+                setError(JSON.stringify(error));
+            }
         }
     );
 
@@ -37,7 +42,7 @@ export default function Login({ done }: { done: () => void }) {
                         <h2 className="py-2 text-red-600">{error}</h2>
                         <FormButton
                             onClick={(e) => {
-                                submit().then(() => done());
+                                submit();
                             }}
                         >
                             Log In
