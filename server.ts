6224c5d9602c91d8be6dbaa219e8eca4a238dca2
Magnus Reeves <rreeves8@uwo.ca>
2023-03-21 16:00:29 -0400

    added https

--- server.ts
+++ server.ts
@@ -6,11 +6,14 @@ import morgan from "morgan";
 import { createRequestHandler } from "@remix-run/express";
 import { authMiddleWare } from "./middleware";
 import "./db/mongodb";
+import fs from "fs";
+import https from "https";
+import http from "http";
 
 dotenv.config();
 
 if (!process.env.type) {
-    throw new Error("no credentials found, need env");
+	throw new Error("no credentials found, need env");
 }
 
 const BUILD_DIR = path.join(process.cwd(), "build");
@@ -33,37 +36,44 @@ app.use(morgan("tiny"));
 app.use(authMiddleWare());
 
 app.all(
-    "*",
-    process.env.NODE_ENV === "development"
-        ? (req: Request, res: Response, next: NextFunction) => {
-              purgeRequireCache();
+	"*",
+	process.env.NODE_ENV === "development"
+		? (req: Request, res: Response, next: NextFunction) => {
+				purgeRequireCache();
 
-              return createRequestHandler({
-                  build: require(BUILD_DIR),
-                  mode: process.env.NODE_ENV,
-              })(req, res, next);
-          }
-        : createRequestHandler({
-              build: require(BUILD_DIR),
-              mode: process.env.NODE_ENV,
-          })
+				return createRequestHandler({
+					build: require(BUILD_DIR),
+					mode: process.env.NODE_ENV,
+				})(req, res, next);
+		  }
+		: createRequestHandler({
+				build: require(BUILD_DIR),
+				mode: process.env.NODE_ENV,
+		  })
 );
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-});
+http.createServer(app).listen(port);
+https
+	.createServer(
+		{
+			key: fs.readFileSync("key.pem"),
+			cert: fs.readFileSync("cert.pem"),
+		},
+		app
+	)
+	.listen(443);
 
 function purgeRequireCache() {
-    // purge require cache on requests for "server side HMR" this won't let
-    // you have in-memory objects between requests in development,
-    // alternatively you can set up nodemon/pm2-dev to restart the server on
-    // file changes, but then you'll have to reconnect to databases/etc on each
-    // change. We prefer the DX of this, so we've included it for you by default
-    for (const key in require.cache) {
-        if (key.startsWith(BUILD_DIR)) {
-            delete require.cache[key];
-        }
-    }
+	// purge require cache on requests for "server side HMR" this won't let
+	// you have in-memory objects between requests in development,
+	// alternatively you can set up nodemon/pm2-dev to restart the server on
+	// file changes, but then you'll have to reconnect to databases/etc on each
+	// change. We prefer the DX of this, so we've included it for you by default
+	for (const key in require.cache) {
+		if (key.startsWith(BUILD_DIR)) {
+			delete require.cache[key];
+		}
+	}
 }
